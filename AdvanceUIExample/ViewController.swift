66fd0c8462852a5b360e00df013dd286ae3065aa
broMac <yhs4705@naver.com>
2022-05-07 06:44:09 +0900

    Project Create & UI 배치

--- AdvanceUIExample/ViewController.swift
+++ AdvanceUIExample/ViewController.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+/*
+ Button Device 수평 수직 중앙
+ Button, label 등은 컨텐츠 크기에 영향을 받는다
+ 
+ 코드로 디바이스의 종류를 알 수 있ㄲ을까?
+ 1. width, height를 통해서 알 수 있다. : UIScreen.main.bouds.width
+ 2. device property
+ 3. device library
+ 
+ */
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
